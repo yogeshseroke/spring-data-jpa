e762358003e4bf1402e41eb74f4e67bdb9101a99
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-04-26 14:51:27 +0530

    Add files via upload

---
+++ spring-data-jpa-springboot/src/main/java/com/dev2pro/springdatajpa/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.dev2pro.springdatajpa.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "emp")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long empId;
+    private String name;
+    private Integer age;
+    @Column(name = "PHONE_NUMBER")
+    private Long phoneNumber=(long) (Math.random()*Math.pow(10,10));
+    private String designation;
+    private Double salary = Math.random()*100000;
+}
